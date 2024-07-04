6c14c64840df630bd31fab55b76a1dafd9a3c072
k3st <kiddoturtle04@gmail.com>
2024-07-04 23:59:13 +0800

    delete product container from HTML when clicking delete

--- amazon/javascript-amazon-project-main/scripts/checkout.js
+++ amazon/javascript-amazon-project-main/scripts/checkout.js
@@ -18,7 +18,8 @@ products.forEach((product)=> {
 console.log(matchingProduct);
 
 cartSummaryHTML += 
-  `<div class="cart-item-container">
+  `<div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
 <div class="delivery-date">
   Delivery date: Wednesday, June 15
 </div>
@@ -97,11 +98,16 @@ cartSummaryHTML +=
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
-document.querySelectorAll('.js-delete-link').forEach((link) => {
+document.querySelectorAll('.js-delete-link')
+.forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
 
-    console.log(cart);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+      );
+
+    container.remove();
   })
 })

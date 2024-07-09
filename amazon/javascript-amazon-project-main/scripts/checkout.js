c8ca0b1639e5b0db25cbfa43445a364b9da6e520
k3st <kiddoturtle04@gmail.com>
2024-07-09 23:57:04 +0800

    using async and await for asynchronous codes

--- amazon/javascript-amazon-project-main/scripts/checkout.js
+++ amazon/javascript-amazon-project-main/scripts/checkout.js
@@ -1,15 +1,26 @@
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProductsFetch } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import "../data/cart-class.js";
 // import "../data/backend-practice.js";
 
-new Promise(()=>{
-    console.log('promise')
-})
+async function loadPage(){
+    console.log('load page');
 
-loadProducts(()=>{
+    await loadProductsFetch();
+    renderOrderSummary();
+    renderPaymentSummary();
+
+}
+loadPage()
+
+/*
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve();
+    });
+}).then(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 })
-
+*/

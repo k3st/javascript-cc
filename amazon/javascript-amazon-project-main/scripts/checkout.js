d0f2977577c1d6c31d203f6ca17c84f59f835664
k3st <kiddoturtle04@gmail.com>
2024-07-09 23:24:32 +0800

    load products from backend

--- amazon/javascript-amazon-project-main/scripts/checkout.js
+++ amazon/javascript-amazon-project-main/scripts/checkout.js
@@ -1,8 +1,15 @@
+import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import "../data/cart-class.js";
-import "../data/backend-practice.js";
+// import "../data/backend-practice.js";
 
-renderOrderSummary();
+new Promise(()=>{
+    console.log('promise')
+})
 
-renderPaymentSummary();
+loadProducts(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+
